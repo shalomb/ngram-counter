8bea563a43cfd72027f80bdbc381872b71feb0e6
Shalom Bhooshi <s.bhooshi@gmail.com>
2019-04-06 23:38:19 +0200

    Initial commit

---
+++ bigram.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+
+class NgramParser:
+  """
+  Ngram parser utility class
+  """
+
+  @classmethod
+  def parse(klass, str):
+    """
+    Parse a string and decompose it into ngrams
+    """
+    print(str)
+    pass
+
+
+def main():
+  bigram = NgramParser.parse('The quick brown fox and the quick blue hare.')
+
+
+if __name__ == '__main__':
+  main()
+
